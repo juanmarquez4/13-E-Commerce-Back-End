839d34c509ea07c54a696a4822d5e6954d146393
Juan Marquez <juanmarquez4@gmail.com>
2022-08-18 18:24:38 +1000

    submission update

--- models/index.js
+++ models/index.js
@@ -16,20 +16,12 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through:{
-    model: ProductTag,
-    unique: false
-  },
-  as: 'product_tags',
+  through:ProductTag,
   foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through:{
-    model: ProductTag,
-    unique: false
-  },
-  as: 'product_tags',
+  through: ProductTag,
   foreignKey: 'tag_id'
 })
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,6 +3,8 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+
+
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
